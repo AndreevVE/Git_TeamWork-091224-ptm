30b3a1e88db3a52172618d57b4b6e845d59f9a2c
anaseller <seller.anst@gmail.com>
2025-03-25 09:42:07 +0100

    Добавлен функционал копирования файлов с изменением расширения

--- filemover.sh
+++ filemover.sh
@@ -13,3 +13,15 @@
 # Проверка, есть ли файлы с указанным расширением в исходной директории
 
 # Копирование файлов с указанным расширением в целевую директорию
+
+
+for file in "$source_directory"/*."$file_extension"; do
+    if [ -f "$file" ]; then
+        
+        new_file_name="$(basename "$file" ."$file_extension").$new_file_extension"
+         
+        cp "$file" "$target_directory/$new_file_name"
+        
+        echo "Файл $file скопирован как $target_directory/$new_file_name"
+    fi
+done
